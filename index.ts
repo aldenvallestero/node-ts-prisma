ad1562f6ec96730738f3600b32fc2836bdd51e3f
Alden Vallestero <alden.v@agtuary.com>
2024-01-18 15:09:52 +0800

    ✨ Demo Prisma

---
+++ index.ts
@@ -0,0 +1,22 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  const newUser = await prisma.user.create({
+    data: {
+      name: 'Alden',
+      email: 'alden@prisma.io',
+      password: 'pass123'
+    },
+  })
+  console.log('Created new user: ', newUser)
+
+  const allUsers = await prisma.user.findMany()
+  console.log('All users: ')
+  console.dir(allUsers, { depth: null })
+}
+
+main()
+  .catch((e) => console.error(e))
+  .finally(async () => await prisma.$disconnect())
